2d1a5bebdcab55b87d86e707781d99f7742371dc
shashidharhr <shashidhar.hr@gmail.com>
2016-01-11 22:11:14 +0530

    Fixed the navigation issue and added the sign-up page

--- app/pages/hello-ionic-signup/hello-ionic-signup.js
+++ app/pages/hello-ionic-signup/hello-ionic-signup.js
@@ -1,17 +1,17 @@
-import {Page, NavController} from 'ionic/ionic';
-import {Injectable} from "angular2/core";
+import {IonicApp, Page, NavController, NavParams} from 'ionic/ionic';
 
 @Page({
   templateUrl: 'build/pages/hello-ionic-signup/hello-ionic-signup.html'
 })
-
-
 export class HelloIonicSignupPage {
+  constructor(app: IonicApp, nav: NavController) {
+    this.nav = nav;
+    // If we navigated to this page, we will have an item available as a nav param
 
+  }
 
+  signUp(){
 
-  constructor(nav: NavController ) {
-     this.nav = nav;
-
+    alert('We still dont have a back end, so this should do fine :)') 
   }
 }
--- app/pages/hello-ionic/hello-ionic.js
+++ app/pages/hello-ionic/hello-ionic.js
@@ -1,8 +1,9 @@
 import {IonicApp, Page, NavController} from 'ionic/ionic';
 import {Http, Response} from 'angular2/http';
 import {Injectable} from "angular2/core";
-  //  import {HelloIonicSignupPage} from './hello-ionic-signup/hello-ionic-signup';
-   import {HelloIonicSignupPage} from '../pages/hello-ionic-signup/hello-ionic-signup';
+
+    import {ItemDetailsPage} from '../item-details/item-details';
+   import {HelloIonicSignupPage} from '../hello-ionic-signup/hello-ionic-signup';
 import 'rxjs/Rx';
 
 //import {Injectable} from 'angular2/angular2';
@@ -41,16 +42,25 @@ export class HelloIonicPage {
         //  alert("Longitude: "+ this.result.coord.lat);
         //  alert("Latitude: "+ this.result.coord.lat);
 
-//  this.nav.push(HelloIonicSignupPage);
+  this.nav.push(HelloIonicSignupPage);
 //-------------------------------------
-  //       this.http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0')
-  // .map((res: Response) => res.json())
-  // .subscribe(
-  //   res => this.result = res,
-  //   err => this.logError(err),
-  //   () => console.log('Fetched successfully')
-  // );
 
+
+   }
+
+   apiCall(){
+
+           this.http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0')
+     .map((res: Response) => res.json())
+     .subscribe(
+       res => this.result = res,
+       err => this.logError(err),
+       () => console.log('Fetched successfully')
+     );
+       console.log("Location:" this.result.name);
+       console.log("Longitude: "this.result.coord.lon);
+       console.log("Latitude: "  this.result.coord.lat);
+       console.log("Visibility" this.result.visibility);
    }
 
 }
